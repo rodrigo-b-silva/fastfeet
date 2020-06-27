9b35db442c9a28c2178f4cf1aad741bbe9d4a57f
rodrigo.b.silva <rodrigo.barbosa@fcamara.com.br>
2020-06-27 18:18:44 -0300

    estrutura inicial e config db

---
+++ backend/src/config/database.js
@@ -0,0 +1,12 @@
+module.expoorts = {
+  dialect: 'postgres',
+  host: '127.0.0.1',
+  username: 'postgres',
+  password: 'admin',
+  database: 'fastfeet',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
